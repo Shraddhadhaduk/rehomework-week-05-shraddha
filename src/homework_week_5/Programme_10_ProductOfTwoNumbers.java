9e1ea9affeb0da1d6b94a79d28a6216355523941
Shraddhadhaduk <shraddhadhaduk96@gmail.com>
2023-10-19 08:41:19 +0100

    rehomework

---
+++ src/homework_week_5/Programme_10_ProductOfTwoNumbers.java
@@ -0,0 +1,21 @@
+package homework_week_5;
+
+/**
+ * 10.Write a Java Program that takes two number as input and display the product of two numbers.
+ * Test Data:
+ * Input first Number: 25
+ * Input Second Number: 5
+ * Expected output : 25 x 5 = 125
+ */
+public class Programme_10_ProductOfTwoNumbers {
+    public static void main(String[] args) {
+        //First Number declaration
+        int firstNumber = 25;
+        //Second Number declaration
+        int secondNumber = 5;
+        int produced = firstNumber * secondNumber;
+        //Expected Output
+        System.out.println(firstNumber + " x " + secondNumber + " = " + produced);
+
+    }
+}
